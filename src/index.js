9ab40800b454648c45f6458eb006fd7d8a8b5c50
Luis Fernando <luisfernandoalves182@gmail.com>
2023-10-27 02:10:27 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,86 @@
+/* Instruções para entrega
+ # 3️⃣ Escrevendo as classes de um Jogo
+
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+- Funções
+- Classes e Objetos
+
+## Objetivo:
+
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+- nome
+- idade
+- tipo (ex: guerreiro, mago, monge, ninja )
+
+além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+se mago -> no ataque exibir (usou magia)
+se guerreiro -> no ataque exibir (usou espada)
+se monge -> no ataque exibir (usou artes marciais)
+se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}"
+  ex: mago atacou usando magia
+  guerreiro atacou usando espada
+ 
+
+ 
+Bons estudos 😉 */
+
+
+class Hero {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+  
+    atacar() {
+      let ataque = "";
+      switch (this.tipo) {
+        case "mago":
+          ataque = "usou magia";
+          break;
+        case "guerreiro":
+          ataque = "usou espada";
+          break;
+        case "monge":
+          ataque = "usou artes marciais";
+          break;
+        case "ninja":
+          ataque = "usou shuriken";
+          break;
+        default:
+          ataque = "usou um ataque desconhecido";
+          break;
+      }
+  
+      console.log(`O ${this.tipo} atacou ${ataque}`);
+    }
+  }
+  
+  
+  const heroi1 = new Hero("Aragorn", 30, "guerreiro");
+  const heroi2 = new Hero("Gandalf", 60, "mago");
+  const heroi3 = new Hero("Naruto", 20, "ninja");
+  const heroi4 = new Hero("Kung Fu Panda", 32, "monge");
+  
+  heroi1.atacar(); 
+  heroi2.atacar(); 
+  heroi3.atacar(); 
+  heroi4.atacar(); 
+
